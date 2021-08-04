0cb37dd367528785537f641f76b6422b8570493a
l3edssa <oussamacreed@gmail.com>
2021-08-04 17:29:20 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var scrollpos = window.scrollY;
+var header = document.querySelector(".navbar--container");
+var navbar = document.querySelector(".navbar");
+
+function add_class_on_scroll() {
+    header.classList.add("navbar__hidden");
+    navbar.classList.remove("on-scroll");
+}
+
+function remove_class_on_scroll() {
+    header.classList.remove("navbar__hidden");
+    navbar.classList.add("on-scroll");
+}
+
+window.addEventListener('scroll', function(){ 
+    //Here you forgot to update the value
+    scrollpos = window.scrollY;
+
+    if(scrollpos > 180){
+        remove_class_on_scroll();
+    }
+    else {
+        add_class_on_scroll();
+    }
+    console.log(scrollpos);
+});
